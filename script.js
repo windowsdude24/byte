cb3bf29bd58815381445067b0e692963a278dc0c
windowsdude24 <146532276+windowsdude24@users.noreply.github.com>
2024-05-24 21:07:46 +0200

    Weitere Anpassungen

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-document.getElementById('clickMeButton').addEventListener('click', function() {
-    document.getElementById('message').innerText = 'Button wurde geklickt!';
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Vielen Dank für Ihre Nachricht!');
+    // Hier können Sie weitere Aktionen hinzufügen, wie das Senden der Formulardaten an einen Server
 });
